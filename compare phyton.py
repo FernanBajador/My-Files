c00b688cfe056e2980dbd36b8c7a13d920f8f782
John Fernand Bajador <johnbajador08@gmail.com>
2024-12-14 14:46:49 -1200

    Add files via upload

---
+++ compare phyton.py
@@ -0,0 +1,9 @@
+Num1= int(input("Num1:"))
+Num2= int(input("Num2:"))
+if Num1>Num2:
+    print("Number 1 is greater than Number 2")
+elif Num1<Num2:
+    print("Number 2 is greater than Number 1")
+elif Num1==Num2:1
+    print("They are equal")
+ 
